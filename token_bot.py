fd9545befb9211fbe59054f7653f3e9bfced0b48
Trade-Bot-sys <tradingstrategieswithram@gmail.com>
2025-06-08 16:47:42 +0530

    Update token_bot.py

--- token_bot.py
+++ token_bot.py
@@ -2,8 +2,8 @@ import os, json, time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-#from fyers_apiv3 import accessToken  # ✅ Correct import for v3+
-from fyers_apiv3.FyersApi.accessToken import SessionModel
+from fyers_apiv3 import accessToken  # ✅ Correct import for v3+
+#from fyers_apiv3.FyersApi.accessToken import SessionModel
 import telebot
 
 # --- Load secrets from environment ---
@@ -50,7 +50,7 @@ def refresh_token():
         auth_code = driver.current_url.split("auth_code=")[-1]
         print("✅ Auth code received")
 
-        session = SessionModel(
+        session = accessToken.SessionModel(
             client_id=APP_ID,
             secret_key=APP_SECRET,
             redirect_uri=REDIRECT_URI,
