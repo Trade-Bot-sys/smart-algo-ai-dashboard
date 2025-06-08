fd1f0f3c5bc98ce9d3ab53b4a75616c6d6d5e95b
Trade-Bot-sys <tradingstrategieswithram@gmail.com>
2025-06-08 17:01:54 +0530

    Update token_bot.py

--- token_bot.py
+++ token_bot.py
@@ -2,7 +2,8 @@ import os, json, time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from fyers_apiv3.FyersWeb import accessToken  # ✅ Correct import for v3+
+from fyers_apiv3.accessToken import SessionModel
+#from fyers_apiv3.FyersWeb import accessToken  # ✅ Correct import for v3+
 import telebot
 
 # --- Load secrets from environment ---
